ec7b110e80638b94623ce354b6d73adc077ad81c
nguyenminhtien98 <nguyenminhtienhy98@gmail.com>
2025-02-26 22:35:06 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 "use client"
+import ClientOnly from "./components/ClientOnly";
 import MainLayout from "./layouts/MainLayout";
+import PostMain from "./components/PostMain";
 
 export default function Home() {
     return (
         <MainLayout>
+            <div className="mt-[80px] w-[calc(100%-90px)] max-w-[690px] ml-auto">
+                <ClientOnly>
+                    <PostMain
+                        post={{
+                            id:'123',
+                            user_id:'456',
+                            video_url:'/videos/video_1.mp4',
+                            text:'this is some text',
+                            created_at:'date here',
+                            profile: {
+                                user_id:'456',
+                                name:"User 1",
+                                image: 'htpps://placehold.co/100'
+                            }
+                        }}
+                    ></PostMain>
+                </ClientOnly>
+            </div>
             <div>home</div>
         </MainLayout>
     );
--- app/types.tsx
+++ app/types.tsx
@@ -1,16 +1,75 @@
 export interface RandomUsers {
-    id: string,
-    name: string,
-    image: string
+  id: string;
+  name: string;
+  image: string;
+}
+
+export interface UserContextTypes {
+  user: User | null;
+  register: (name: string, email: string, password: string) => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  checkUser: () => Promise<void>;
+}
+
+export interface User {
+  id: string,
+  name: string,
+  bio: string,
+  image: string,
+}
+
+export interface PostWithProfile {
+  id: string;
+  user_id: string;
+  video_url: string;
+  text: string;
+  created_at: string;
+  profile: {
+    user_id: string;
+    name: string;
+    image: string;
+  };
+}
+
+export interface Like {
+  id: string;
+  user_id: string;
+  post_id: string;
+}
+
+export interface Post {
+  id: string;
+  user_id: string;
+  video_url: string;
+  text: string;
+  created_at: string;
+}
+
+export interface Comment {
+  id: string;
+  user_id: string;
+  post_id: string;
+  text: string;
+  created_at: string;
+}
+
+//Component
+export interface PostMainCompTypes {
+  post: PostWithProfile;
 }
 
 //Layout
 export interface MenuItemTypes {
-    iconString: string,
-    colorString: string,
-    sizeString: string
+  iconString: string;
+  colorString: string;
+  sizeString: string;
 }
 
 export interface MenuItemFollowCompTypes {
-    user: RandomUsers
+  user: RandomUsers;
+}
+
+export interface PostMainLikesCompTypes {
+  post: PostWithProfile
 }
